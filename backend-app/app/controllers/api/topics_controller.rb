90d11f164109ce3d90e5aaebc3afa8e82791044c
tran anh vu <eyelight93@gmail.com>
2017-04-26 21:04:19 +0700

    list topic and question api

---
+++ backend-app/app/controllers/api/topics_controller.rb
@@ -0,0 +1,12 @@
+class Api::TopicsController < ApplicationController
+  def index
+    topics = Topic.all
+    render json: topics
+  end
+
+  def show
+    topic = Topic.find(params[:id])
+    questions = topic.questions
+    render json: questions
+  end
+end
